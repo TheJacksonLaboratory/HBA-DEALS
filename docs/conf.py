6074576085750eb04bfcf8426307116ce2172a10
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-06-14 10:33:32 -0400

    Update the stylesheet path.

--- docs/conf.py
+++ docs/conf.py
@@ -94,6 +94,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['style.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -166,8 +167,3 @@ texinfo_documents = [
      author, 'hbadeals', 'HBA-DEALS',
      'Miscellaneous'),
 ]
-
-
-
-def setup(app):
-    app.add_css_file('style.css')
